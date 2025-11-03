b5ba650b0cf4d6c63bd023d8cb07dfd0a5439d6a
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-11-03 16:33:41 +0000

    Added: created divvy_tripdata_2024

---
+++ divvy_trips/models/raw/divvy_tripdata_2024.sql
@@ -0,0 +1,13 @@
+{%- set months = range(1, 13) -%}
+{%- set table_names = [] -%}
+{%- for month in months -%}
+  {%- do table_names.append("2024" ~ "{:02d}".format(month) ~ "_divvy_tripdata") -%}
+{% endfor %}
+
+{%- for table_name in table_names -%}
+    SELECT *
+    FROM {{ source('divvy_trip_app', table_name) }}
+    {% if not loop.last %}
+      UNION ALL
+    {% endif %}
+{% endfor %}
--- divvy_trips/models/test.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    *
-FROM
-    {{ source('divvy_trip_app', '202404_divvy_tripdata') }}
-LIMIT 5;
